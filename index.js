53c4638de73111e34a5b6134828b4c5ff5cf5b81
actboy168 <actboy168@gmail.com>
2024-01-25 10:14:56 +0800

    support tar

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 const fs = require("fs");
 const path = require("path");
+const process = require('process')
 const core = require("@actions/core");
 const glob = require('@actions/glob');
-const AdmZip = require("adm-zip");
 
 async function run() {
     const workspacePath = process.env.GITHUB_WORKSPACE;
     const argPath = core.getInput("path", { required: true });
     const argName = core.getInput("name", { required: true });
-    core.info(`Zip path: ${argPath}`);
-    const zip = new AdmZip();
     const globber = await glob.create(argPath);
-    for await (const file of globber.globGenerator()) {
-        const stats = fs.lstatSync(file);
-        if (!stats.isDirectory()) {
-            const zippath = path.relative(workspacePath, file);
-            zip.addFile(zippath, fs.readFileSync(file), "", 0);
+    core.info(`Zip path: ${argPath}`);
+    if (process.platform === 'win32') {
+        const argOutput = argName + ".zip";
+        const AdmZip = require("adm-zip");
+        const zip = new AdmZip();
+        for await (const file of globber.globGenerator()) {
+            const stats = fs.lstatSync(file);
+            if (!stats.isDirectory()) {
+                const zippath = path.relative(workspacePath, file);
+                zip.addFile(zippath, fs.readFileSync(file), "", 0);
+            }
         }
+        zip.writeZip(argOutput);
+        core.info(`Zipped file ${argOutput} successfully`);
+        core.setOutput('output', argOutput);
     }
-    const argOutput = path.join(argName, ".zip");
-    zip.writeZip(argOutput);
-    core.info(`Zipped file ${argOutput} successfully`);
-    core.setOutput('output', argOutput);
+    else {
+        const argOutput = argName + ".tar.gz";
+        const tar = require("tar");
+        let files = [];
+        for await (const file of globber.globGenerator()) {
+            const stats = fs.lstatSync(file);
+            if (!stats.isDirectory()) {
+                const zippath = path.relative(workspacePath, file);
+                files.push(zippath);
+            }
+        }
+        tar
+          .c({ cwd: workspacePath, gzip: true, sync: true }, files)
+          .pipe(fs.createWriteStream(argOutput));
+        core.info(`Zipped file ${argOutput} successfully`);
+        core.setOutput('output', argOutput);
+    }
 }
 
 run()
